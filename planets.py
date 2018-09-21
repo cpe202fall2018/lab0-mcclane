0cca249e96e6157b73f65e1b3059d8be53c5d3af
McClane Howland <mcclane.howland@gmail.com>
2018-09-21 11:01:07 -0700

    Only uses 1 print statement now

--- planets.py
+++ planets.py
@@ -1,11 +1,7 @@
 def weight_on_planets():
    # write your code here
-   print("What do you weigh on earth? ", end='')
-   weight = int(input())
-   print("")
-
-   print("On Mars you would weigh "+str(weight*0.38)+" pounds.")
-   print("On Jupiter you would weigh "+str(weight*2.34)+" pounds.")
+   weight = int(input("What do you weigh on earth? "))
+   print("\nOn Mars you would weigh "+str(weight*0.38)+" pounds.\nOn Jupiter you would weigh "+str(weight*2.34)+" pounds.")
 
 if __name__ == '__main__':
    weight_on_planets()
